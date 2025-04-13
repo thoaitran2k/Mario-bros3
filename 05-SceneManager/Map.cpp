3f50474f25e7425bcf111799e5aafc858dd94b82
thoaitran2k <185214555@gm.uit.edu.vn>
2025-04-13 22:18:57 +0700

    initialize project and add map object

---
+++ 05-SceneManager/Map.cpp
@@ -0,0 +1,18 @@
+#include "Map.h"
+#include "debug.h"
+
+
+void CMap::Render()
+{
+	//if (model == 1) {
+	CAnimations* animations = CAnimations::GetInstance();
+	animations->Get(ID_ANI_MAP)->Render(x, y);
+
+	//}
+	//else {
+	//	CAnimations* animations = CAnimations::GetInstance();
+	//	animations->Get(ID_ANI_HIDEN_MAP)->Render(x, y);
+	//}
+}
+
+void CMap::GetBoundingBox(float& l, float& t, float& r, float& b) {}
